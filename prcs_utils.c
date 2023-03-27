3d1d866b17720e281966940a2b2db5b4ea89e30f
azrarafeeq <azraaminah@gmail.com>
2023-03-27 22:29:12 +0400

    rmoved early freeing of env_list

--- prcs_utils.c
+++ prcs_utils.c
@@ -6,7 +6,7 @@
 /*   By: arafeeq <arafeeq@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 19:52:42 by arafeeq           #+#    #+#             */
-/*   Updated: 2023/03/27 19:55:31 by arafeeq          ###   ########.fr       */
+/*   Updated: 2023/03/27 22:28:07 by arafeeq          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ int	parent_process(t_cmd *cmd, int i, t_infra *shell)
 			return (1);
 		ft_built_in(cmd[i], &shell->env_list);
 		free_char_array(shell->env_arr);
-		free_env_list(&shell->env_list);
+		//free_env_list(&shell->env_list);
 	}
 	//free_int_array(shell->pfd, shell->pipe_len);
 	return (0);
